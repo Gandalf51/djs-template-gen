cf1d10224103b067b027471ac0f80fac5393737f
guilherme fernandes <thegodscrew1@gmail.com>
2022-10-14 15:40:10 -0300

    primeira att

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
   const fse = require('fs-extra')
 
-  const sourcebase = `./node_modules/simplebotdiscord/slash`;
+  const sourcebase = `./node_modules/simplebotdiscordjs/slash`;
   const sourcedestino = `./`;
   
-  const sourcebase1 = `./node_modules/simplebotdiscord/prefix`;
+  const sourcebase1 = `./node_modules/simplebotdiscordjs/prefix`;
   const sourcedestino1 = `./`;
                                    
    // fse.readdir("", console.log)
